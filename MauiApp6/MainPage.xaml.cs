fb78600b74ee7c585cfea5e71542074d46f92f00
Lautaro Carro <lcarro@algeiba.com>
2023-02-09 15:46:36 -0300

    Add project files.

---
+++ MauiApp6/MainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿namespace MauiApp6;
+
+public partial class MainPage : ContentPage
+{
+
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+
+
+    private void Button1Btn_Clicked(object sender, EventArgs e)
+    {
+		
+
+        Shell.Current.FlyoutIsPresented = !Shell.Current.FlyoutIsPresented;
+    }
+
+    private void Button2Btn_Clicked(object sender, EventArgs e)
+    {
+        HideLbl.Text = "WorkAround";
+
+        Shell.Current.FlyoutIsPresented = !Shell.Current.FlyoutIsPresented;
+    }
+}
+
